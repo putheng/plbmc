5cd9a2831697111715fdf757047a025ee4beb17c
Putheng <puthengemail@gmail.com>
2018-05-09 11:13:35 +0000

    fixed this on helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,30 +1,30 @@
 <?php
 
-	function after($this, $inthat){
-		if (!is_bool(strpos($inthat, $this))){
-			return substr($inthat, strpos($inthat,$this)+ strlen($this));
+	function after($needed, $inthat){
+		if (!is_bool(strpos($inthat, $needed))){
+			return substr($inthat, strpos($inthat,$needed)+ strlen($needed));
 		}
 	}
 
-	function after_last ($this, $inthat){
-		if (!is_bool(strrevpos($inthat, $this)))
-		return substr($inthat, strrevpos($inthat, $this)+strlen($this));
+	function after_last ($needed, $inthat){
+		if (!is_bool(strrevpos($inthat, $needed)))
+		return substr($inthat, strrevpos($inthat, $needed)+strlen($needed));
 	}
 
-	function before ($this, $inthat){
-		return substr($inthat, 0, strpos($inthat, $this));
+	function before ($needed, $inthat){
+		return substr($inthat, 0, strpos($inthat, $needed));
 	}
 
-	function before_last ($this, $inthat){
-		return substr($inthat, 0, strrevpos($inthat, $this));
+	function before_last ($needed, $inthat){
+		return substr($inthat, 0, strrevpos($inthat, $needed));
 	}
 
-	function between ($this, $that, $inthat){
-		return before ($that, after($this, $inthat));
+	function between ($needed, $that, $inthat){
+		return before ($that, after($needed, $inthat));
 	}
 
-	function between_last ($this, $that, $inthat){
-	 return after_last($this, before_last($that, $inthat));
+	function between_last ($needed, $that, $inthat){
+	 return after_last($needed, before_last($that, $inthat));
 	}
 
 	function strrevpos($instr, $needle){
